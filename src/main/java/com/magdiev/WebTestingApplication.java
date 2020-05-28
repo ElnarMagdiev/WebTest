6da54426632a5239d29c592ec2158e4ec3b1cd88
Магдиев Эльнар <@solomon.local>
2020-05-28 15:20:02 +0300

    'version_1'

---
+++ src/main/java/com/magdiev/WebTestingApplication.java
@@ -0,0 +1,13 @@
+package com.magdiev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magdiev/WebTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magdiev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
