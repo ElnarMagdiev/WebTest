68cfa35183ec29917bd30b01879f06939f7eedbb
Магдиев <elnarmag263@gmail.com>
2020-05-28 16:26:40 +0300

    Adding jdbc

--- src/main/java/com/magdiev/controllers/MainController.java
+++ src/main/java/com/magdiev/controllers/MainController.java
@@ -1,15 +1,25 @@
 package com.magdiev.controllers;
 
+import com.magdiev.dao.QuestionDao;
+import com.magdiev.models.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @Autowired
+    private QuestionDao dao;
+
+    @RequestMapping("/")
     public String index(Model model) {
+        List<Question> listQuestion = dao.allQuestions();
         model.addAttribute("title", "Web testing");
+        model.addAttribute("listQuestion", listQuestion);
         return "index";
     }
 }
--- src/main/java/com/magdiev/dao/QuestionDaoImpl.java
+++ src/main/java/com/magdiev/dao/QuestionDaoImpl.java
@@ -1,6 +1,9 @@
 package com.magdiev.dao;
 
 import com.magdiev.models.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,9 +11,20 @@ import java.util.List;
 @Repository
 public class QuestionDaoImpl implements QuestionDao {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public QuestionDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Question> allQuestions() {
-        return null;
+        String sql = "SELECT * FROM questions";
+        List<Question> listQuestion = jdbcTemplate.query(sql,
+                BeanPropertyRowMapper.newInstance(Question.class));
+
+        return listQuestion;
     }
 
     @Override
